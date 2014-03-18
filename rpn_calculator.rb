766a95f261fb4c0f7e966f294c9e3632157ae567
jongmini <jongmin.choi@gmail.com>
2014-03-17 18:13:31 -0700

    rpn lab tests passed

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -6,14 +6,32 @@ class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    rpn_string.split(" ")
   end
 
   def self.evaluate(rpn_list)
+    stack = Stack.new()
+    rpn_list.each do |x| 
+      if self.is_operation(x)
+        stack.push(x)
+        operator = stack.pop
+        val2 = (stack.pop).to_f
+        val1 = (stack.pop).to_f
+        res = val1.public_send(operator,val2)
+        stack.push(res)
+      else
+        stack.push(x)
+      end
+    end
+    stack.pop
   end
 
   def self.is_operation(operation)
+    operation.match(/[\*+-\/\^]/) ? true : false
   end
   
   def self.is_number(num)
+    num.match(/\A[+-]?\d+?(_?\d+)*(\.\d+e?\d*)?\Z/) ? true : false
+
   end
 end
--- stack.rb
+++ stack.rb
@@ -1,5 +1,89 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
+# require './list.rb'
 
 class Stack
+
+  attr_reader :length
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+    # @list = List.new
+  end
+
+  # Adds a new value to the end of the list.
+  def push(value)
+    @node = Node.new(value)
+    if @head == nil
+      @head = @node
+      @tail = @head
+    else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+    end
+    @length += 1
+    self
+  end
+
+  # Returns the value that is popped off
+  # or nil if none exists
+  def pop
+    if @length == 0 
+      return nil
+    else
+      old_tail = @tail
+      if @tail.previous.nil?
+        @head = nil
+        @tail = nil
+      else
+        @tail = old_tail.previous
+        @tail.next = nil
+      end
+      @length -= 1
+    end
+    old_tail.value
+  end
+
+  def insert(index, value)
+  end
+
+  # Given an index, returns the value at that index
+  def [](index)
+
+  end
+
+  # Sets a value at the given index.  Returns the value that
+  # was assigned
+  def []=(index, value)
+    value
+  end
+
+  # Returns the first value that was removed from the list and
+  # shifts all items down by 1
+  def shift
+  end
+
+  # Adds a new value to the front of the list
+  def unshift(value)
+  end
+
+  def first
+  end
+
+  def last
+  end
+
+end
+
+class Node
+
+  attr_accessor :next, :previous, :value
+
+  def initialize(value)
+    @value = value
+  end
 end
